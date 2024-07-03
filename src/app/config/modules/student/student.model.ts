b7c76be64c548f98a252868e50e9da563f0712a0
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2024-07-04 01:29:12 +0600

    create a student full complete

--- src/app/config/modules/student/student.model.ts
+++ src/app/config/modules/student/student.model.ts
@@ -1,6 +1,10 @@
-import mongoose, { Schema } from "mongoose";
-import { Guardian, LocalGuardian, Student, UserName } from "./student.interface";
-
+import { Schema, model } from "mongoose";
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from "./student.interface";
 
 const GuardianSchema: Schema = new Schema<Guardian>({
   fatherName: { type: String, required: true },
@@ -44,5 +48,5 @@ const StudentSchema: Schema = new Schema<Student>({
   profileImg: { type: String },
   isActive: { type: String, enum: ["active", "inactive"], required: true },
 });
-
-export default mongoose.model<Student & Document>("Student", StudentSchema);
+export const StudentModel = model<Student>("Student", StudentSchema);
+// export default mongoose.model<Student & Document>("Student", StudentSchema);
